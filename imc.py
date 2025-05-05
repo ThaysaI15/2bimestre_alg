e434f8fd1fc92e6c15fb3a4cf01a395d55b322db
Thaysa Miyasak Rezende <thaysaif2008@gmail.com>
2025-05-05 15:17:44 -0400

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,21 @@
+def calculadora_imc(pessoa):
+    imc = pessoa["peso"] / (pessoa["altura"] * pessoa["altura"])
+    resultado = (f"O IMC {pessoa} é {imc:.2f}")
+    #comando tenário
+    saudavel = 18.5 < imc < 25
+
+    return {
+        "nome": pessoa["nome"],
+        "imc": imc,
+        "resultado": resultado,
+        "saudavel": saudavel
+    }
+
+pessoa1={
+    "nome":"josé",
+    "peso": 110,
+    "altura": 1.75
+}
+
+print(calculadora_imc(pessoa1))
+# print(calculadora_imc(pessoa1))
---
+++ variaveis.py
@@ -0,0 +1,9 @@
+status = "maravilhoso"
+
+def comportamento():
+    global status
+    status = "cansativo"
+    print("Pytho é " + status)
+
+comportamento()
+print("Pytho é " + status)
