2ab1019f12f71b28fb02914f5da8f0dd8f5cab9c
Thaysa Miyasak Rezende <thaysaif2008@gmail.com>
2025-05-26 15:18:56 -0400

    Add files via upload

--- testando_definicoes.py
+++ testando_definicoes.py
@@ -1,14 +1,29 @@
-compras = ["pão", "café", "leite"]
-print (compras)
-compras.remove(compras[0]) ##pode ser removido pelo nome ou pelo índice
-print (compras)
-compras.append("açúcar") ##append só pode adicionar um por vez
-print (compras)
-# compras.sort()
+# compras = ["pão", "café", "leite"]
 # print (compras)
-#é preciso criar uma lista nova para receber os elementos rdenados na lista antiga
-compras_ordenada = sorted(compras)
-print (compras_ordenada)
+# compras.remove(compras[0]) ##pode ser removido pelo nome ou pelo índice
+# print (compras)
+# compras.append("açúcar") ##append só pode adicionar um por vez
+# print (compras)
+# # compras.sort()
+# # print (compras)
+# #é preciso criar uma lista nova para receber os elementos rdenados na lista antiga
+# compras_ordenada = sorted(compras)
+# print (compras_ordenada)
 
-for item in compras:
-    print("-", item)
+# for item in compras:
+#     print("-", item)
+
+# listas = []
+# dicionários = {chave: valor}
+# tuplas = {}
+# conjuntos = set()
+
+# teste = [1, 2, 3, 5]
+# print(teste)
+# teste.append(4)
+# print(teste)
+
+teste = {1, 2, 3, 5}
+print(teste)
+teste.remove{1}
+print(teste)
