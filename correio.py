681a84905bd7c42aa328e7f66df1a5633beb7034
Thaysa Miyasak Rezende <thaysaif2008@gmail.com>
2025-06-09 13:23:45 -0400

    Add files via upload

--- correio.py
+++ correio.py
@@ -1,7 +1,7 @@
 pacotes = ("ABC123", "XYZ789", "DEF456", "JKL321", "MNO654", "PQR987", "STU741")
 rastreio = ("Enviado", "Recebido", "Em Trânsito", "Enviado", "Recebido", "Em Trânsito", "Enviado")
 indices = []
-pacotes2 = []
+pacotes2 = [pacotes]
 # print(len(pacotes))
 print("Enviados:", rastreio.count("Enviado"))
 print("Recebido:", rastreio.count("Recebido"))
@@ -9,13 +9,10 @@ print("Em Trânsito", rastreio.count("Em Trânsito"))
 
 for i in rastreio:
     if i == "Em Trânsito":
-        indices.append(pacotes(i))
-        # indices = (pacotes[i])
-        # pacotes2.append(indices)
-        # pacotes2 = pacotes[indices]
+        indices.append(pacotes2[i])
 
-print("Pacotes em trânsito:", pacotes2)
+# print("Pacotes em trânsito:", pacotes2)
 
-rastr = input("Insira o código de rastreamento para ver os status do pacote: ")
-if rastr in pacotes:
-    print("Posição da primeira ocorrência do número 7:", numero.index(7))
+# rastr = input("Insira o código de rastreamento para ver os status do pacote: ")
+# if rastr in pacotes:
+#     print("Posição da primeira ocorrência do número 7:", numero.index(7))
