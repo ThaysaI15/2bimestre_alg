a2fc5f084b758bfbcf7e656887bd7bb666ea1a05
Thaysa Miyasak Rezende <thaysaif2008@gmail.com>
2025-06-09 21:19:48 -0400

    Add files via upload

--- correio.py
+++ correio.py
@@ -1,21 +1,29 @@
-pacotes = ("ABC123", "XYZ789", "DEF456", "JKL321", "MNO654", "PQR987", "STU741")
-rastreio = ("Enviado", "Recebido", "Em Trânsito", "Enviado", "Recebido", "Em Trânsito", "Enviado")
-indices = []
-pacotes2 = [pacotes]
-# print(len(pacotes))
-print("Enviados:", rastreio.count("Enviado"))
-print("Recebido:", rastreio.count("Recebido"))
-print("Em Trânsito", rastreio.count("Em Trânsito"))
+pacotes = (("ABC123", "Enviado"),("XYZ789", "Recebido"),("DEF456", "Em Trânsito"),("JKL321", "Enviado"),("MNO654", "Recebido"),("PQR987", "Em Trânsito"),("STU741", "Enviado"))
 
-for i in rastreio:
-    if rastreio[i] == "Em Trânsito":
-        indices.append(pacotes2[i])
-print ("Pacotes em Trânsito: ", indices)
+cont = {"Enviado": 0, "Recebido": 0, "Em Trânsito": 0}
 
-rastreamento = input("insira o código do rastreamento: ")
-for i in pacotes:
-    if rastreamento == pacotes[i]:
-        indice = i
-        print ("O status do pacote é: ", rastreio[indice])
-    if rastreamento != pacotes[i]:
-        print ("Pacote não está cadastrado")
+for codigo, status in pacotes:
+    cont[status] = cont[status]+1
+
+print("Contagem de pacotes por status: ")
+print("Enviado:", cont["Enviado"])
+print("Recebido:", cont["Recebido"])
+print("Em Trânsito:", cont["Em Trânsito"])
+
+transito = (cont["Em Trânsito"])
+
+print("\nPacotes em trânsito:", transito,"\n")
+
+def pesq(rastreamento, pacotes):
+    if codigo.upper() == rastreamento.upper():
+            return f"O pacote '{rastreamento}' está com status: {status}"
+    return "Pacote não cadastrado."
+
+codigo = input("Digite o código de rastreamento do pacote: ").strip()
+print(pesq(codigo, pacotes))
+
+pacotes_ordenados = (sorted(pacotes))
+
+print("\nPacotes Ordenados:\n")
+for pacotes in pacotes_ordenados:
+    print(pacotes)
